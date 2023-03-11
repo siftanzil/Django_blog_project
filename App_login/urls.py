f185b1137095666d51919bb4aa5d083616daffce
siftanzil <siftanzil@gmail.com>
2023-03-12 00:17:41 +0600

    login, profile info change

--- App_login/urls.py
+++ App_login/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("signup/", views.sign_up, name="register"),
     path('login/', views.sign_in, name="login"),
     path('logout/', views.sign_out, name="logout"),
+    path('profile/', views.profile, name="profile"),
+    path('change_profile/', views.change_profile, name="change_profile"),
+    path('password/', views.change_pass, name="change_pass"),
 ]
--- App_login/views.py
+++ App_login/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
+from App_login.forms import SignUpForm, UserProfileChange
 
 # Create your views here.
 
 
 def sign_up(request):
-    form = UserCreationForm()
+    form = SignUpForm()
     registered = False
     if request.method == "POST":
-        form = UserCreationForm(data=request.POST)
+        form = SignUpForm(data=request.POST)
         if form.is_valid():
             form.save()
             registered = True
@@ -38,3 +39,33 @@ def sign_in(request):
 def sign_out(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
+
+
+@login_required
+def profile(request):
+    return render(request, "App_login/profile.html", context={})
+
+
+@login_required
+def change_profile(request):
+    current_user = request.user
+    form = UserProfileChange(data=request.POST, instance=current_user)
+    if request.method == 'POST':
+        form = UserProfileChange(data=request.POST, instance=current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance=current_user)
+    return render(request, "App_login/change_profile.html", context={'form': form})
+
+
+@login_required
+def change_pass(request):
+    current_user = request.user
+    changed = False
+    form = PasswordChangeForm(current_user)
+    if request.method == 'POST':
+        form = PasswordChangeForm(current_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            changed = True
+    return render(request, "App_login/change_pass.html", context={'form': form, 'changed': changed})
