b2f4f174ce65d464b47caea73cdb786a9788adf2
siftanzil <siftanzil@gmail.com>
2023-03-13 02:13:17 +0600

    Post and comment

--- App_Blog/models.py
+++ App_Blog/models.py
@@ -15,6 +15,9 @@ class Blog(models.Model):
     publish_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-publish_date']
+
     def __str__(self):
         return self.blog_title
 
@@ -27,6 +30,9 @@ class Comment(models.Model):
     comment = models.TextField()
     comment_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-comment_date']
+
     def __str__(self):
         return self.comment
 
@@ -36,3 +42,6 @@ class Like(models.Model):
         Blog, on_delete=models.CASCADE, related_name='liked_blog')
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='liker_user')
+
+    def __str__(self):
+        return self.user + " likes " + self.blog
--- App_Blog/urls.py
+++ App_Blog/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'App_Blog'
 
 urlpatterns = [
-    path('', views.blog_list, name='blog_list')
+    path('', views.BlogList.as_view(), name='blog_list'),
+    path('create_blog/', views.CreateBlog.as_view(), name="create_blog"),
+    path('details/<slug>', views.blog_details, name='blog_details'),
 ]
